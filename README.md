06df2be22e1cbdc3efe8ed6e1605d60fc6de6fcb
xknower <xknower@126.com>
2023-11-23 11:56:25 +0800

    全局格式化及代码规范处理

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 > 该项目用于，对 jdk8 项目进行研究学习分析，对源码进行注释梳理。在项目 [jdk8u](https://github.com/openjdk/jdk8u) 基础上，对其 java 源码目录 jdk\src\share\classes 建立版本跟踪并进行研究学习和分析。
 
+- 郑重声明：本项目来自互联网，仅供个人研究学习整理之用，版权归发行公司所有，任何组织和个人不得公开传播或用于任何商业盈利用途，否则一切后果由该组织或个人承担。本站和制作者不承担任何法律及连带责任！联系侵删！
+
 - [jdk8u](https://github.com/openjdk/jdk8u) commit id e74d29d67429a8059ec168882ee3b540dea67a1f, Severin Gehwolf <sgehwolf@openjdk.org> on 2023/9/11 at 16:09, 8295894: Remove SECOM certificate that is expiring in September 2023
-- [jdk8u](https://github.com/xknower/jdk8u) | [jdk8u-study](https://github.com/xknower/)
+- [jdk8u](https://github.com/xknower/jdk8u) | [jdk8u-study](https://github.com/xknower/jdk8u-study)
--- jdk/internal/org/xml/sax/XMLReader.java
+++ jdk/internal/org/xml/sax/XMLReader.java
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
-// XMLReader.java - read an XML document.
-// http://www.saxproject.org
-// Written by David Megginson
-// NO WARRANTY!  This class is in the Public Domain.
-// $Id: XMLReader.java,v 1.3 2004/11/03 22:55:32 jsuttor Exp $
-
 package jdk.internal.org.xml.sax;
 
 import java.io.IOException;
